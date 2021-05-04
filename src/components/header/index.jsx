ea7eb1fe8bdf96be36db6cffc6b60f6bbd9e17f8
steve karuma <stevekaruma@gmail.com>
2021-05-04 18:03:57 +0300

    toggle mobile menu

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,44 +1,73 @@
+import React, {useState} from "react"
 import {Link} from "react-router-dom"
-
 import Logo from "assets/svg/logo.svg"
 import LogoAlt from "assets/svg/logo-white.svg"
 
 function MobileNav(){
+    const [menuState, setMenuState] = useState(false)
+
+    const toggleMobileMenu = () =>{
+        if (menuState == false){
+            setMenuState(true)
+        } else {
+            setMenuState(false)
+        }
+    }
+
     return(
         <>
-            <div id="navButton">
-                <span><i className="fas fa-bars"></i></span>
+            <div id="navButton" onClick={toggleMobileMenu}>
+                {
+                    menuState ? (
+                        <>
+                            <span><i className="fas fa-times"></i></span>
+                        </>
+                    ) : (
+                        <>
+                            <span><i className="fas fa-bars"></i></span>
+                        </>
+                    )
+                }
             </div>
+            
+            {
+                menuState ? (
+                    <>
+                        <div id="mobileNav" className="p-3 border-box">
+                            <div id="mobileMenu" className="text-white d-flex flex-column">
+                                <span className="my-2"><Link to="recipes">Recipes</Link></span>
+                                <span className="my-2"><Link to="">Video</Link></span>
+                                <span className="my-2"><Link to="">Gallery</Link></span>
+                                <span className="my-2"><Link to="">Cookouts</Link></span>
+                            </div>
 
-            <div id="mobileNav" className="p-3 border-box">
-                <div id="mobileMenu" className="text-white d-flex flex-column">
-                    <span className="my-2"><Link to="recipes">Recipes</Link></span>
-                    <span className="my-2"><Link to="">Video</Link></span>
-                    <span className="my-2"><Link to="">Gallery</Link></span>
-                    <span className="my-2"><Link to="">Cookouts</Link></span>
-                </div>
+                            <div id="navFooter">
+                                <div id="logoFooter">
+                                    <img src={LogoAlt} alt="white logo" width="160"/>
+                                </div>
+                                
+                                <div id="socialsFooter" className="text-white mb-5">
+                                    <span className="d-inline-block mx-2"><a href="#" ><i className="fab fa-md text-white fa-instagram"></i> </a></span>
+                                    <span className="d-inline-block mx-2"><a href="#" ><i className="fab fa-md text-white fa-twitter"></i> </a></span>
+                                    <span className="d-inline-block mx-2"><a href="#" ><i className="fab fa-md text-white fa-youtube"></i> </a></span>
+                                    <span className="d-inline-block mx-2"><a href="#" ><i className="fab fa-md text-white fa-facebook"></i> </a></span>
+                                </div>
 
-                <div id="navFooter" className="border-box">
-                    <div id="logoFooter">
-                        <img src={LogoAlt} alt="white logo" width="160"/>
-                    </div>
-                    
-                    <div id="socialsFooter" className="text-white mb-5">
-                        <span className="d-inline-block mx-2"><a href="#" ><i className="fab fa-md text-white fa-instagram"></i> </a></span>
-                        <span className="d-inline-block mx-2"><a href="#" ><i className="fab fa-md text-white fa-twitter"></i> </a></span>
-                        <span className="d-inline-block mx-2"><a href="#" ><i className="fab fa-md text-white fa-youtube"></i> </a></span>
-                        <span className="d-inline-block mx-2"><a href="#" ><i className="fab fa-md text-white fa-facebook"></i> </a></span>
-                    </div>
-
-                    <div id="copyrightIcons" className="">
-                        <span>
-                            <p className="text-white text-sm">
-                                © 2021
-                            </p>
-                        </span>
-                    </div>
-                </div>
-            </div>
+                                <div id="copyrightIcons" className="">
+                                    <span>
+                                        <p className="text-white text-sm">
+                                            © 2021
+                                        </p>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                    </>
+                )
+            }
         </>
     )
 }
